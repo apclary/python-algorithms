bb9d44c266f13eab08cd27b2b14ea91f4cd7a3f2
apclary <apclary@gmail.com>
2015-10-10 13:29:33 -0500

    Added merge sort
    Added the merge sort and test. Also bumped up the test size to 5000
    elements to get a better idea of comparative performance between
    sorting algorithms.

--- tests/test_merge_sort.py
+++ tests/test_merge_sort.py
@@ -2,20 +2,20 @@ import unittest
 import random
 import copy
 
-from python_algorithms import insertion_sort
+from python_algorithms import merge_sort
 from tests import print_timing, test_sort
 
 
 class TestInsertionSort(test_sort.TestSort):
 
     @print_timing.print_timing
-    def test_insertion_sort(self):     
+    def test_merge_sort(self):     
         # Generate an unsorted and sorted list of the same values
         # to verify a sort worked
         unsorted_list = random.sample(range(1000000), 5000) 
         sorted_list = copy.copy(unsorted_list).sort()
-
-        self.assertEqual(insertion_sort.insertion_sort(unsorted_list), sorted_list)
+        
+        self.assertEqual(merge_sort.merge_sort(unsorted_list), sorted_list)
 
 if __name__ == '__main__':
     unittest.main()

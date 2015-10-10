6f3bb7c2d4008172134eac5ab25f1f247ea14a95
apclary <apclary@gmail.com>
2015-10-09 21:18:37 -0500

    Implemented insertion sort

--- python_algorithms/insertion_sort.py
+++ python_algorithms/insertion_sort.py
@@ -2,4 +2,10 @@
 # insertion sort implementation
 
 def insertion_sort(list):
-    return list.sort()
+    for j in range(1,len(list)):
+        key = list[j]
+        i = j - 1
+        while i > 0 and list[i] > key:
+            list[i+1] = list[i]
+            i = i - 1
+        list[i+1] = key
--- tests/test_sort.py
+++ tests/test_sort.py
@@ -2,6 +2,6 @@ import unittest
 
 class TestSort(unittest.TestCase):
     pass
-    
+        
 if __name__ == '__main__':
     unittest.main()

ae2f36dfdce88fe0391274136bcd0a390936ac0e
apclary <apclary@gmail.com>
2015-10-09 21:33:42 -0500

    Improving comments in insertion_sort and README.md

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-github.com/apclary
-run tests with python -m unittest discover
+[![Build Status](https://travis-ci.org/apclary/python-algorithms.svg?branch=master)](https://travis-ci.org/apclary/python-algorithms)
+
+## Python Algorithms
+
+This is intended to be a collection of Python implementations of various CLRS algorithms, just for fun and practice. It's been awhile since I've taken CS classes so I want to refresh my memory. I also wanted to gain some experience with Python testing and Travis CI.
+
+Run tests with `python -m unittest discover` from the `python-algorithms` directory.
--- python_algorithms/insertion_sort.py
+++ python_algorithms/insertion_sort.py
@@ -1,11 +1,25 @@
 # apclary
-# insertion sort implementation
+# Insertion sort implementation
+# CLRS 2.1
 
 def insertion_sort(list):
     for j in range(1,len(list)):
+        # The element to be inserted into the sorted section of the list
         key = list[j]
-        i = j - 1
+        # The comparison element is set to the end of the sorted section of the list
+        i = j - 1 
+        
+        # Proceed downward through the sorted section
+        # to find an element which is less than the key.
+        # That location + 1 must be the correct spot for 
+        # the key. Shift the rest of the elements forward
+        # by one to make room.
         while i > 0 and list[i] > key:
+            # Shift elements forward
             list[i+1] = list[i]
             i = i - 1
-        list[i+1] = key
+            
+        # This is the position where the elment before is lower and 
+        # the element above is higher, so this is the correct spot to
+        # place it.
+        list[i+1] = key 

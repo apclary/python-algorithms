acee70210b1fbf6c595993c04c9a64ec4d26026c
apclary <apclary@gmail.com>
2015-10-12 19:56:43 -0500

    Added heap sort
    Added heap sort implementation using a max heap

--- python_algorithms/heapsort.py
+++ python_algorithms/heapsort.py
@@ -4,21 +4,88 @@ Heap sort implementation
 CLRS 6.1
 """
 
-#todo
-__all__ = ['methods i want to expose']
+class MaxHeap:
 
-def left(i):
-    return 2 * i
+    def __init__(self):
+        self.heap_size = 0
+        self.heap = []
+        
+    @staticmethod
+    def _left(i):
+        return 2 * i + 1
     
-def right(i):
-    return 2 * i + 1
+    @staticmethod
+    def _right(i):
+        return 2 * i + 2
     
-def parent(i):
-    return i / 2
+    @staticmethod
+    def _parent(i):
+        #return int(i-1 / 2)
+        return int((i+1)/2) - 1
+     
+    def _swap(self,i,j):
+        #print("swapping",i+1,"(",self.heap[i],") and",j+1,"(",self.heap[j],")")
+        temp = self.heap[i]
+        self.heap[i] = self.heap[j]
+        self.heap[j] = temp
 
-def max_heapify(lst, i):
-    l = left(i)
-    r = right(i)
+    def _max_heapify(self, i):
+        l = self._left(i)
+        r = self._right(i)
+        largest = None
+        
+        """"
+        print("")
+        print("i is",i+1,"(",self.heap[i],")")
+        if (l < self.heap_size):
+            print("left is",self.heap[l])
+        else:
+            print("left is null")
+        if (r < self.heap_size):
+            print("right is",self.heap[r])
+        else:
+            print("right is null")
+        
+        if (i != 0):
+            print("parent is",self._parent(i)+1,"(",self.heap[self._parent(i)],")")
+        else:
+            print("parent is null (root node)")
+        print("")
+        """
+        
+        
+        if l < self.heap_size and self.heap[l] > self.heap[i]:
+            largest = l
+        else:
+            largest = i
+            
+        if r < self.heap_size and self.heap[r] > self.heap[largest]:
+            largest = r
+        
+        if largest != i:
+            self._swap(i,largest)
+            self._max_heapify(largest)
     
+    def _build_max_heap(self):
+        self.heap_size = len(self.heap)
+        pos_last_non_leaf_node = int(self.heap_size/2) - 1
+        for i in range(pos_last_non_leaf_node, -1, -1):
+            #print("calling _max_heapify for",i+1)
+            self._max_heapify(i)
+        
+    def set_heap(self, new_heap):
+        self.heap = new_heap
+        self.heap_size = len(new_heap)
+        
+    def sort(self):
+        self._build_max_heap()
+        for i in range(len(self.heap)-1,0,-1):
+            self._swap(0,i)
+            self.heap_size -= 1
+            self._max_heapify(0)
+            
+        
+            
+            
     
     
--- tests/test_heapsort.py
+++ tests/test_heapsort.py
@@ -2,7 +2,7 @@ import unittest
 import random
 import copy
 
-from python_algorithms import heapsort
+from python_algorithms.heapsort import MaxHeap
 from tests import print_timing, test_sort
 
 
@@ -12,10 +12,46 @@ class TestInsertionSort(test_sort.TestSort):
     def test_heapsort(self):     
         # Generate an unsorted and sorted list of the same values
         # to verify a sort worked
+
         unsorted_list = random.sample(range(1000000), 500) 
-        sorted_list = copy.copy(unsorted_list).sort()
+        sorted_list = copy.copy(unsorted_list)
+        sorted_list.sort()
+        heap = MaxHeap()
+        heap.set_heap(unsorted_list)
+        heap.sort()
+        self.assertEqual(heap.heap, sorted_list)
+
+    def test_init(self):
+        heap = MaxHeap()
+        self.assertEqual(heap.heap_size, 0)
+    
+    def test_max_heapify(self):
+        heap = MaxHeap()
+        heap.heap = [16,4,10,14,7,9,3,2,8,1]
+        heap.heap_size = len(heap.heap)
+        heap._max_heapify(1)
+        self.assertEqual(heap.heap, [16,14,10,8,7,9,3,2,4,1]) 
+         
+    def test_set_heap(self):
+        heap = MaxHeap()
+        lst = [16,14,10,8,7,9,3,2,4,1]
+        heap.set_heap(lst)
+        self.assertEqual(heap.heap, lst)
         
-        self.assertEqual(heapsort.heapsort(unsorted_list), sorted_list)
+    def test_build_max_heap(self):        
+        heap = MaxHeap()
+        heap.heap = [16,4,10,14,7,9,3,2,8,1]
+        heap._build_max_heap()
+        self.assertEqual(heap.heap, [16,14,10,8,7,9,3,2,4,1])
+        
+        heap.set_heap([4,1,3,2,16,9,10,14,8,7])
+        heap._build_max_heap()
+        self.assertEqual(heap.heap, [16,14,10,8,7,9,3,2,4,1]) 
+
+        
+        
+        
+       
 
 if __name__ == '__main__':
     unittest.main()
